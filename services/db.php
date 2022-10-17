fcdfcc76d0c7d63ad731e712ac883170f84024a1
sams3wa <samy95800@hotmail.com>
2022-10-17 09:42:04 +0000

    mehdi

---
+++ services/db.php
@@ -0,0 +1,27 @@
+<?php
+
+const DB_HOST = 'db.3wa.io';
+const DB_PORT = '3306';
+const DB_NAME = 'maximilienmbaye_projetFramework';
+const DB_USERNAME = 'maximilienmbaye';
+const DB_PASSWORD = '030da881f38decf9259cd3d2df897fb0';
+
+
+function getConnect(): \PDO
+{
+    try
+{
+    return new PDO(
+       'mysql:host='. DB_HOST . ';port=' . DB_PORT . ';dbname='. DB_NAME,
+       DB_USERNAME,
+       DB_PASSWORD
+   ) ;
+}
+catch (Exception $e)
+{
+die('Erreur : ' . $e->getMessage());
+}
+   
+}
+
+
